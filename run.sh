8dee83b432ec450a7e24da2cd37b2c4d51cc9629
stanaka27 <stanaka0627@gmail.com>
2025-04-25 13:23:13 +0200

    to operate run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash  
+
+# Install R packages listed in requirements.txt
+while read pkg; do
+    Rscript -e "if (!requireNamespace('$pkg', quietly = TRUE)) install.packages('$pkg', repos='https://cloud.r-project.org')"
+done < requirements.txt
+
 ./kodaqs_assignment_D2.1.sh
